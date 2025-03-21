e609da616e545091bb56048edc3707e82874cb98
mkarasinski <maciej.karasinski.95@gmail.com>
2025-03-21 13:43:33 +0100

    counter - injector, destruction of effects

--- signals-playground/src/app/app.component.ts
+++ signals-playground/src/app/app.component.ts
@@ -2,12 +2,24 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BasicSignalsComponent } from './basic-signals/basic-signals.component';
 import { LinkedSignalsComponent } from './linked-signals/linked-signals.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 @Component({
-  imports: [BasicSignalsComponent, CommonModule, LinkedSignalsComponent],
+  imports: [
+    BasicSignalsComponent,
+    CommonModule,
+    CounterComponent,
+    LinkedSignalsComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  showCounter = false;
+
+  toggleCounter() {
+    this.showCounter = !this.showCounter;
+  }
+}
---
+++ signals-playground/src/app/components/counter/counter.component.ts
@@ -0,0 +1,51 @@
+import {
+  Component,
+  effect,
+  EffectRef,
+  inject,
+  Injector,
+  signal,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss',
+})
+export class CounterComponent {
+  readonly value = signal(0);
+  private ef: EffectRef | null = null;
+  private readonly injector = inject(Injector);
+
+  constructor() {
+    setInterval(() => {
+      this.value.update((v) => v + 1);
+    }, 1000);
+  }
+
+  go(): void {
+    if (this.ef) {
+      return;
+    }
+
+    this.ef = effect(
+      () => {
+        console.log(this.value());
+      },
+      {
+        // to use effects after initialization, we need to pass injector
+        // effect will be automatically destroyed when component is destroyed
+        injector: this.injector,
+      }
+    );
+  }
+
+  stop(): void {
+    // to stop effect manually
+    if (this.ef) {
+      this.ef.destroy();
+      this.ef = null;
+    }
+  }
+}
