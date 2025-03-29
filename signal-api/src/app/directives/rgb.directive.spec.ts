eff0b7089cb5b8fa57a9a6ea96d6ab8b8c5d2b42
mkarasinski <maciej.karasinski.95@gmail.com>
2025-03-29 13:19:08 +0100

    host binding with signals, rgb text directive

---
+++ signal-api/src/app/directives/rgb.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RgbDirective } from './rgb.directive';
+
+describe('RgbDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RgbDirective();
+    expect(directive).toBeTruthy();
+  });
+});
